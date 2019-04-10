8cdac6f1c8fdff57b9088be8cd78c2960e16bf43
Shailen <shailen@gettruck.co.za>
2019-04-10 17:33:09 +0200

    Created data transformers

--- src/components/adapters/DepartmentPreviewAdapter/transformData.ts
+++ src/components/adapters/DepartmentPreviewAdapter/transformData.ts
@@ -1,10 +1,48 @@
 import { TrouterProps, Tresponse, Tdata } from './schema';
-import { departments, dropdownItems } from '../../views/DepartmentPreview/schema';
+import { TdropdownItem, TdepartmentForPreview } from '../../views/DepartmentPreview/schema';
+import { Tdepartment } from '../../../data/schemas/departments';
 
-const transformData = (response: Tresponse, routerObject: TrouterProps): Tdata => {
+const createDropdownItems = ({ items }: { items: Tdepartment[] }): TdropdownItem[] =>
+  items.map(
+    ({ title: name, slug: id }): TdropdownItem => ({
+      name,
+      id,
+    }),
+  );
+
+const checkIfNationalOrProvincial = (sphere, government): string =>
+  sphere === 'south-africa' ? '' : government;
+
+const createURL = ({ year, sphere, government, department }: TrouterProps): string =>
+  `/${year}/previews/${sphere}/${checkIfNationalOrProvincial(sphere, government)}/${department}`;
+
+const createNewDepartments = (
+  { items }: { items: Tdepartment[] },
+  routerObject: TrouterProps,
+): TdepartmentForPreview[] =>
+  items.map(
+    ({
+      title: name,
+      slug: id,
+      percentage_of_budget: percentage,
+      year,
+      programmes,
+      ...otherValues
+    }): TdepartmentForPreview => ({
+      ...otherValues,
+      name,
+      id,
+      percentage,
+      url: createURL({ year, ...routerObject, department: id }),
+      programmes,
+    }),
+  );
+
+// @ts-ignore
+const transformData = ({ data: { data } }: Tresponse, routerObject: TrouterProps): Tdata => {
   return {
-    dropdownItems,
-    departments,
+    dropdownItems: createDropdownItems(data),
+    departments: createNewDepartments(data, routerObject),
   };
 };
 
