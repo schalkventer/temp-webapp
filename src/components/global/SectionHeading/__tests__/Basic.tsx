dc5fadea95bf441d3bf976bac4de1cf2b5720354
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-12 15:49:51 +0200

    commiting work in progress

---
+++ src/components/global/SectionHeading/__tests__/Basic.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import SectionHeading from '../index';
+import mock from '../schema';
+
+const Default = (): JSX.Element => <SectionHeading {...mock} />;
+
+export default Default;
