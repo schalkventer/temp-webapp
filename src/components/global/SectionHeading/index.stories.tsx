a8c286cb9e383c52370040200ed38e6c72755e3b
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-12 15:46:53 +0200

    start building story

--- src/components/global/SectionHeading/index.stories.tsx
+++ src/components/global/SectionHeading/index.stories.tsx
@@ -4,18 +4,13 @@ import { storiesOf } from '@storybook/react';
 import SectionHeading from './index';
 
 const share = () => (
-  <SectionHeading title="Provincial Budget Summary" share>
-    Children Components
-  </SectionHeading>
+  <SectionHeading title="Provincial Budget Summary" share />
 );
 const noShare = () => (
-  <SectionHeading title="Provincial Budget Summary">Children Components</SectionHeading>
+  <SectionHeading title="Provincial Budget Summary" />
 );
 const shareString = () => (
-  <SectionHeading title="Provincial Budget Summary" share="addTextHere">
-    Children Components
-  </SectionHeading>
-);
+  <SectionHeading title="Provincial Budget Summary" share="addTextHere" />
 
 storiesOf('component.SectionHeading', module)
   .add('Share', share)
