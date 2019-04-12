2c87d6f0822a366caa747a64375204564adb0ee0
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-12 12:55:59 +0200

    Add stories for Demo and Basic tests

--- src/components/global/ResourceList/__tests__/index.stories.tsx
+++ src/components/global/ResourceList/__tests__/index.stories.tsx
@@ -2,14 +2,14 @@ import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import content from '../README.md';
-import Demo from './Demo';
-import Basic from './Basic';
+import { Demo, DemoWithCite } from './Demo';
+import { Basic, BasicWithCite } from './Basic';
 
 storiesOf('⚙️ components|global/ResourceList', module)
   .add('README', (): JSX.Element => <Fragment />, { readme: { content } })
-  .add('Demo', (): JSX.Element => <Demo />);
+  .add('Demo', (): JSX.Element => <Demo />)
+  .add('DemoWithCite', (): JSX.Element => <DemoWithCite />);
 
-storiesOf('⚙️ components|global/ResourceList/tests', module).add(
-  'Basic',
-  (): JSX.Element => <Basic />,
-);
+storiesOf('⚙️ components|global/ResourceList/tests', module)
+  .add('Basic', (): JSX.Element => <Basic />)
+  .add('BasicWithCite', (): JSX.Element => <BasicWithCite />);
