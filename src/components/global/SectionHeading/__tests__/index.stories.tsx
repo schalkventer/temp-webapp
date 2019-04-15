2d5a44f5889a7731ce961f242c6cc39c7b6d7b2a
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-15 15:44:25 +0200

    remove old story and create new one

--- src/components/global/SectionHeading/__tests__/index.stories.tsx
+++ src/components/global/SectionHeading/__tests__/index.stories.tsx
@@ -4,14 +4,16 @@ import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import content from '../README.md';
-import Demo from './Demo';
-import Basic from './Basic';
+import { Demo, DemoNoShare, DemoShareIsAString } from './Demo';
+import { Basic, BasicNoShare, BasicShareIsAString } from './Basic';
 
 storiesOf('⚙️ components|global/SectionHeading', module)
   .add('README', (): JSX.Element => <Fragment />, { readme: { content } })
-  .add('Demo', (): JSX.Element => <Demo />);
+  .add('Demo', (): JSX.Element => <Demo />)
+  .add('DemoNoShare', (): JSX.Element => <DemoNoShare />)
+  .add('DemoShareIsAString', (): JSX.Element => <DemoShareIsAString />);
 
-storiesOf('⚙️ components|global/SectionHeading/tests', module).add(
-  'Basic',
-  (): JSX.Element => <Basic />,
-);
+storiesOf('⚙️ components|global/SectionHeading/tests', module)
+  .add('Basic', (): JSX.Element => <Basic />)
+  .add('BasicNoShare', (): JSX.Element => <BasicNoShare />)
+  .add('BasicShareIsAString', (): JSX.Element => <BasicShareIsAString />);
