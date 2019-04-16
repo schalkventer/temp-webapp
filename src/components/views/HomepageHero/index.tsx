44912042bef55f3c0e22ee1a3726c0657299fceb
Yauheni <guest@guests-MacBook-Pro.local>
2019-04-16 14:30:02 +0300

    Styled HomeHero Component

--- src/components/views/HomepageHero/index.tsx
+++ src/components/views/HomepageHero/index.tsx
@@ -12,7 +12,19 @@ import {
   HomeHeaderMiddleCentered,
   HeaderTitle,
   HeaderQuestion,
+  ButtonsArea,
+  ButtonYellow,
+  TextInButton,
+  ArrowBlack,
+  BottomWrapper,
+  ButtonInfo,
+  InfoImageButton,
+  ArrowForwardLeftButton,
+  Cell,
+  ImageFirstRight,
+  HeaderTopWrapper,
 } from './styled';
+import ImageFirst from './GroupImages.png';
 
 const Presentation = (props: Tprops): JSX.Element => (
   <div>
@@ -28,12 +40,41 @@ const Presentation = (props: Tprops): JSX.Element => (
       <HomeHeaderMiddleWrapper>
         <HomeHeaderMiddleCentered>
           <HeaderLeftWrapper>
+            <HeaderTopWrapper>
+              <Cell>
+                <ImageFirstRight src={ImageFirst} alt="img1" />
+              </Cell>
+            </HeaderTopWrapper>
             <HeaderTitle>Discover how government spends taxpayer money</HeaderTitle>
             <HeaderQuestion>Which budget would you like to explore?</HeaderQuestion>
+            <ButtonsArea>
+              <ButtonYellow>
+                <TextInButton>Consolidated</TextInButton>
+                <ArrowBlack />
+              </ButtonYellow>
+              <ButtonYellow>
+                <TextInButton>National</TextInButton>
+                <ArrowBlack />
+              </ButtonYellow>
+              <ButtonYellow>
+                <TextInButton>Provincial</TextInButton>
+                <ArrowBlack />
+              </ButtonYellow>
+            </ButtonsArea>
           </HeaderLeftWrapper>
-          <HeaderLeftWrapper />
-          <HeaderRightWrapper>Lorem</HeaderRightWrapper>
+          <HeaderRightWrapper>
+            <Cell>
+              <ImageFirstRight src={ImageFirst} alt="img1" />
+            </Cell>
+          </HeaderRightWrapper>
         </HomeHeaderMiddleCentered>
+        <BottomWrapper>
+          <ButtonInfo>
+            <InfoImageButton />
+            What is the difference between the Consolidated, National and Provincial budgets?
+            <ArrowForwardLeftButton />
+          </ButtonInfo>
+        </BottomWrapper>
       </HomeHeaderMiddleWrapper>
     </Wrapper>
   </div>
--- src/components/views/HomepageHero/styled.ts
+++ src/components/views/HomepageHero/styled.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 import { Typography, Button } from '@material-ui/core';
 import arrow from './ArrowYellowForward.svg';
+import ArrowForward from './ArrowForward.svg';
 import middleBackgroundImage from './HomeMiddleBg.svg';
+import InfoImage from './info.svg';
+import ArrowForwardLeft from './ArrowForwardRight.svg';
 
 const Wrapper = styled.div`
   display: flex;
@@ -12,10 +15,13 @@ const HomeHeaderTopWrapper = styled.div`
   background: #2d2d2d;
   display: flex;
   height: 44px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const TitleWrapper = styled.div`
-  width: 80%;
+  width: 99%;
   justify-content: flex-start;
   margin: auto;
 `;
@@ -60,10 +66,11 @@ const HomeHeaderMiddleWrapper = styled.div`
 const HomeHeaderMiddleCentered = styled.div`
   display: flex;
   flex-direction: row;
-  width: 80%;
+  width: 99%;
   margin: 0 auto;
   @media screen and (max-width: 768px) {
     flex-direction: column;
+    width: 95%;
   }
 `;
 
@@ -79,11 +86,38 @@ const HeaderLeftWrapper = styled.div`
 
 const HeaderRightWrapper = styled.div`
   display: flex;
+  flex-direction: row;
+  position: relative;
   width: 50%;
   @media screen and (max-width: 768px) {
     width: 100%;
     flex-direction: column;
+    display: none;
   }
+  @media screen and (max-width: 1380px) {
+    align-items: center;
+  }
+`;
+
+const HeaderTopWrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const Cell = styled.div`
+  flex: 1 1 auto;
+  margin-top: 50px;
+  text-align: center;
+  @media screen and (max-width: 768px) {
+    margin-top: 0;
+  }
+`;
+
+const ImageFirstRight = styled.img`
+  position: relative;
+  max-width: 100%;
+  width: 100%;
 `;
 
 const HeaderTitle = styled(Typography)`
@@ -92,6 +126,13 @@ const HeaderTitle = styled(Typography)`
     font-size: 48px;
     font-weight: bold;
     margin-top: 80px;
+    @media screen and (max-width: 1080px) {
+      font-size: 36px;
+    }
+    @media screen and (max-width: 768px) {
+      font-size: 32px;
+      margin-top: 0;
+    }
   }
 `;
 
@@ -100,7 +141,104 @@ const HeaderQuestion = styled(Typography)`
     width: 100%;
     font-size: 24px;
     margin-top: 20px;
+    @media screen and (max-width: 1080px) {
+      font-size: 18px;
+    }
   }
+`;
+
+const ButtonsArea = styled.div`
+  display: flex;
+  @media screen and (max-width: 1080px) {
+    flex-direction: column;
+    margin-top: 10px;
+  }
+`;
+
+const ButtonYellow = styled(Button)`
+  && {
+    display: flex;
+    flex-direction: row;
+    height: 64px;
+    margin-right: 10px;
+    margin-top: 20px;
+    background: #eca03e;
+    font-weight: bold;
+    text-transform: capitalize;
+    padding-left: 25px;
+    &:hover {
+      background: #eca03e;
+    }
+    @media screen and (max-width: 768px) {
+      margin-top: 0;
+      margin-bottom: 10px;
+      margin-right: 0;
+    }
+  }
+`;
+
+const TextInButton = styled(Typography)`
+  && {
+    font-size: 20px;
+    font-weight: bold;
+    @media screen and (max-width: 1080px) {
+      position: absolute;
+      left: 25px;
+    }
+  }
+`;
+
+const ArrowBlack = styled.img`
+  background-image: url('${ArrowForward}');
+  background-repeat: no-repeat;
+  background-position: 95% 49.5%;
+  width: 16px;
+  padding: 10px;
+  margin-right: 13px;
+  @media screen and (max-width: 1080px){
+    position: absolute;
+    right: 10px;
+  }
+`;
+
+const BottomWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 99%;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 95%;
+  }
+`;
+
+const ButtonInfo = styled(Button)`
+  && {
+    margin: 20px 0;
+    font-size: 16px;
+    text-transform: capitalize;
+    padding: 20px;
+    background-color: #f7f7f7;
+    -webkit-box-shadow: 0px 9px 20px -5px rgba(0, 0, 0, 0.4);
+    box-shadow: 0px 9px 20px -5px rgba(0, 0, 0, 0.4);
+    &:hover {
+      background-color: #f7f7f7;
+    }
+    @media screen and (max-width: 768px) {
+      text-align: left;
+    }
+  }
+`;
+
+const InfoImageButton = styled.img`
+  background-image: url('${InfoImage}');
+  padding: 10px;
+  margin: 0px 15px 0 0;
+`;
+
+const ArrowForwardLeftButton = styled.img`
+  background-image: url('${ArrowForwardLeft}');
+  padding: 8px;
+  margin: 0px 0 0 15px;
 `;
 
 export {
@@ -116,6 +254,17 @@ export {
   HeaderRightWrapper,
   HeaderTitle,
   HeaderQuestion,
+  ButtonsArea,
+  TextInButton,
+  ButtonYellow,
+  ArrowBlack,
+  BottomWrapper,
+  ButtonInfo,
+  InfoImageButton,
+  ArrowForwardLeftButton,
+  ImageFirstRight,
+  Cell,
+  HeaderTopWrapper,
 };
 
 export default {
@@ -131,4 +280,15 @@ export default {
   HeaderRightWrapper,
   HeaderTitle,
   HeaderQuestion,
+  ButtonsArea,
+  TextInButton,
+  ButtonYellow,
+  ArrowBlack,
+  BottomWrapper,
+  ButtonInfo,
+  InfoImageButton,
+  ArrowForwardLeftButton,
+  ImageFirstRight,
+  Cell,
+  HeaderTopWrapper,
 };
